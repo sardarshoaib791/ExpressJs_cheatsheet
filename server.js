404d4d55887ed29464627cd620cc4d90cfe238fe
Sardar Shoaib <sardarshoaib910@gmail.com>
2024-07-21 23:10:56 +0500

    basic routs

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-
 const path = require("path");
 const { env } = require("process");
+const PORT = process.env.PORT || 3000;
+
+// app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   //   res.sendFile(path.join(__dirname, "/index.html"));   Alternative way
@@ -13,7 +15,6 @@ app.get("/about", (req, res) => {
   res.sendFile(path.resolve(__dirname) + "/about.html");
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log("listing on port 3000");
 });
