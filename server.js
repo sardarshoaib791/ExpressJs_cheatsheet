2d73cc19eb7b179265f7b957285ad9faa20b478b
Sardar Shoaib <sardarshoaib910@gmail.com>
2024-07-22 13:02:32 +0500

    Ejs engin set

--- server.js
+++ server.js
@@ -1,25 +1,40 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const { env } = require("process");
+const { env, title } = require("process");
 const PORT = process.env.PORT || 3000;
 
-// app.use(express.static("public"));    use extention in path href eg ".hmtl"
+app.set("view engine", "ejs");
+
+// console.log(app.get("view engin"));   //check engin
+// console.log(app.get("views")); //file  check path
+app.use(express.static("public")); //use extention in path href eg ".hmtl"
 
 app.get("/", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "/index.html"));   Alternative way
-  res.sendFile(path.resolve(__dirname) + "/index.html");
+  res.render("index", {
+    title: "Home page",
+  });
 });
 app.get("/about", (req, res) => {
-  //   res.sendFile(path.join(__dirname, "/index.html"));   Alternative way
-  res.sendFile(path.resolve(__dirname) + "/about.html");
+  res.render("about", {
+    title: "About page",
+  });
 });
+
+// app.get("/", (req, res) => {
+//   //   res.sendFile(path.join(__dirname, "/index.html"));   Alternative way
+//   res.sendFile(path.resolve(__dirname) + "/index.html");
+// });
+// app.get("/about", (req, res) => {
+//   //   res.sendFile(path.join(__dirname, "/index.html"));   Alternative way
+//   res.sendFile(path.resolve(__dirname) + "/about.html");
+// });
 
 // download in express
 app.get("/download", (req, res) => {
   res.download(path.resolve(__dirname) + "/about.html");
 });
 
-app.listen(3000, () => {
-  console.log("listing on port 3000");
+app.listen(PORT, () => {
+  console.log(`Listing on port ${PORT}`);
 });
