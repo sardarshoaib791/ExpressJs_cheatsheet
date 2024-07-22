34bcef2d55dbb0efc40fe96fa0393c3920a73ce6
Sardar Shoaib <sardarshoaib910@gmail.com>
2024-07-22 18:30:17 +0500

    user api key validation

--- middleware/apiKey.js
+++ middleware/apiKey.js
@@ -1,7 +1,14 @@
 function apikey(req, res, next) {
   const api_key = "12345678";
-  console.log(req.query);
-  next();
+  const userApiKey = req.query.api_key;
+  console.log(userApiKey);
+  if (userApiKey && userApiKey === api_key) {
+    next();
+  } else {
+    res.json({
+      message: "not vallid api",
+    });
+  }
 }
 
 module.exports = apikey;
